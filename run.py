a9e2a8f66c2702d78e1ddb803041a9a4b8ab8f75
addas3 <addas3code@gmail.com>
2023-04-10 16:02:46 +0000

    Creat a define function for the game. having the welcome message and the rules and basic structure

--- run.py
+++ run.py
@@ -8,6 +8,19 @@ def get_word():
     word = random.choice(list_word)
     return word.upper()
 
+def game(word):
+    completion_word = "_" * len(word)
+    guessed = False
+    words_guessed = []
+    letters_guessed = []
+    attempts = 6
+    print("Welcome to Hangman!")
+    print("Rules are simple guess the word before the hangman is fully drawn!")
+    print(hangman_display(attempts))
+    print(completion_word)
+    print("Let's start the game!")
+    print("\n")
+
 
 
 
