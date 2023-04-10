441baa461d9d5e1131fe206120e26e61ca8a377d
addas3 <addas3code@gmail.com>
2023-04-10 15:44:38 +0000

    Define function to get word from the list of words and returning it to uppercase

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 import random
 from listwords import list_word
+
+def get_word():
+    """
+    Getting the word from the listword and chaning it to uppercase
+    """
+    word = random.choice(list_word)
+    return word.upper()
+
+
+
+
