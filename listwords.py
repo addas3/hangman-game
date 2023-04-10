a56e47239e42f5b6652395f632f7ff66da80adac
addas3 <addas3code@gmail.com>
2023-04-10 15:36:15 +0000

    Creat list word and import to run.py, also import random

---
+++ listwords.py
@@ -0,0 +1,16 @@
+list_word = [ 
+    'apple',
+    'banana', 
+    'blackberry', 
+    'cherry', 
+    'lemon', 
+    'kiwi', 
+    'orange', 
+    'grapes', 
+    'lime', 
+    'mango', 
+    'peach',
+    'pineapple',
+    'raisins',
+    'strawberry',
+    'watermelon']
